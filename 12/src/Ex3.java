91bb0eeef8f977fc76f0f9599b58db6e402a44f3
llliizzz <sotnikova2308@yandex.ru>
2025-11-18 15:24:14 +0300

    3exercise

--- 12/src/Ex3.java
+++ 12/src/Ex3.java
@@ -1,23 +1,23 @@
-import java.io.*;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Ex3 {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("12/input.txt");
-             FileOutputStream out = new FileOutputStream("output.txt")) {
-            byte[] buffer = new byte[in.available()];
-            in.read(buffer, 0, buffer.length);
-            out.write(buffer, 0, buffer.length);
-        } catch (FileNotFoundException e) {
-            if (e.getMessage().contains("input.txt")) {
-                System.out.println("input file not found, " + e.getMessage());
-            } else if (e.getMessage().contains("output.txt")) {
-                System.out.println("output file not found");
-            } else {
-                System.out.println(e.getMessage());
+        try (Scanner scanner = new Scanner(new File("12/input.txt"))) {
+
+            int num1 = scanner.nextInt();
+            int num2 = scanner.nextInt();
+
+            try {
+                int result = num1 / num2;
+                System.out.println("result: " + result);
+            } catch (ArithmeticException e) {
+                System.out.println("division by zero is not allowed");
             }
-        } catch (IOException e) {
-            System.out.println("no write-permission for the output file" + e.getMessage());
+
+        } catch (FileNotFoundException e) {
+            System.out.println("input file not found: " + e.getMessage());
         }
     }
 }
